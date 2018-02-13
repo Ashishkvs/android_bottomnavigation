879da5e0a7fec2a792a14845b75ed4c6536643a1
Ashish <ashishkvs@gmail.com>
2018-02-13 09:17:49 +0530

    Added actionbar

--- app/src/main/java/datazi/com/bottomnavigation/HomeFragment.java
+++ app/src/main/java/datazi/com/bottomnavigation/HomeFragment.java
@@ -3,10 +3,13 @@ package datazi.com.bottomnavigation;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -22,7 +25,18 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-      return inflater.inflate(R.layout.fragment_home,null);
+      View view= inflater.inflate(R.layout.fragment_home,null);
+        Button button=view.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(),"Hello there",Toast.LENGTH_SHORT).show();
+            }
+        });
+      return view;
+    }
+    public void showInfo(View view ){
+        Toast.makeText(view.getContext(),"Hi this is view",Toast.LENGTH_SHORT).show();
     }
 
 }
--- app/src/main/java/datazi/com/bottomnavigation/MainActivity.java
+++ app/src/main/java/datazi/com/bottomnavigation/MainActivity.java
@@ -4,9 +4,15 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
 
@@ -14,6 +20,19 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //App bar Toolbar
+         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+         setSupportActionBar(myToolbar);
+
+         // Get a support ActionBar corresponding to this toolbar
+         ActionBar ab = getSupportActionBar();
+
+         // Enable the Up button
+         ab.setDisplayHomeAsUpEnabled(true);
+
+
+
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(this);
@@ -52,6 +71,36 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
                 break;
         }
         return loadFragment(fragment);
+    }
+
+    //set menu item visible to action bar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater=getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    //App bar menu selected
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                // User chose the "Settings" item, show the app settings UI...
+                return true;
+
+            case R.id.action_favorite:
+                // User chose the "Favorite" action, mark the current item
+                // as a favorite...
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 }
 
